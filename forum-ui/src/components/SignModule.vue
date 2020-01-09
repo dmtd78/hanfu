3c7b39dcab0b812f2d3399b32c4d802131645387
dmtd78 <ljxr78.ok@163.com>
2020-01-09 23:12:52 +0800

    我的积分

--- forum-ui/src/components/SignModule.vue
+++ forum-ui/src/components/SignModule.vue
@@ -2,11 +2,11 @@
     <a-card hoverable style="width: 240px">
         <a-card-meta>
             <div :style="{ width: '240px', border: '1px solid #d9d9d9', borderRadius: '4px' }">
-                
+
             </div>
         </a-card-meta>
 
-        <a-card-meta title="" description="我的积分：500">
+        <a-card-meta title="" :description="data.integral">
 
         </a-card-meta>
 
@@ -14,13 +14,34 @@
 </template>
 
 <script>
+    import axios from 'axios';
+    // eslint-disable-next-line no-irregular-whitespace
+    const userId = JSON.parse(sessionStorage.getItem("userId"));
     export default {
         name: "SignModule",
-        methods:{
-            onPanelChange(value, mode) {
+        mounted() {
+            this.getData(res => {
+                this.loading = false;
+                this.data = res.data.data;
                 // eslint-disable-next-line no-console
-                console.log(value, mode);
-            }
+                console.log('get data :'+this.data);
+            });
+        },
+        methods: {
+            getData(callback) {
+                if (userId == null) {
+                    return;
+                }
+                axios.get('/integral/getIntegralByUserId', {params: {uid: userId}}, {
+                    xhrFields: {
+                        withCredentials: true
+                    },
+                }).then((res) => {
+                    if (res.data.resultCode == 0) {
+                        callback(res);
+                    }
+                })
+            },
         }
     }
 </script>
