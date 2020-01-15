e0034f6f2eee5989f824ac3e4d026b7fdae6f52c
duanmin <ljxr78.ok@163.com>
2020-01-15 16:09:15 +0800

    添加提示

--- forum-ui/src/components/AddForum.vue
+++ forum-ui/src/components/AddForum.vue
@@ -4,6 +4,7 @@
             发帖
         </a-button>
         <a-modal
+                width="80%"
                 :visible="visible"
                 title='创建一个新帖'
                 okText='创建'
@@ -13,10 +14,10 @@
         >
             <a-form layout='vertical' :form="form">
                 <a-form-item>
-                    <a-radio-group buttonStyle="solid" v-model="value" v-decorator="[ 'type']">
-                        <a-radio-button value="1">闲聊</a-radio-button>
-                        <a-radio-button value="2">图文赏析</a-radio-button>
-                        <a-radio-button value="3">小白</a-radio-button>
+                    <a-radio-group name="type" :defaultValue="1" v-decorator="['type']">
+                        <a-radio-button :value="1">闲聊</a-radio-button>
+                        <a-radio-button :value="2">图文赏析</a-radio-button>
+                        <a-radio-button :value="3">小白</a-radio-button>
                     </a-radio-group>
                 </a-form-item>
                 <a-form-item label='标题'>
@@ -25,6 +26,13 @@
                 </a-form-item>
                 <a-form-item label='内容'>
                     <a-input v-decorator="['description']" v-model="description" v-show="false"></a-input>
+                    <a-tooltip title="注：目前，如果帖子内容包含图片的话，请将图片大小控制在20kb以内，以后升级服务器会无此限制，非常感谢您的谅解！
+                    ②为了营造一个轻松的汉服交流空间，帖子内容请慎重考虑，不要涉及一些有影响的不健康的还有国家禁止的内容，非常感谢！">
+                        <a-icon type="question-circle-o"/>
+                    </a-tooltip>
+                    <span style="color: gray">注：①目前，如果帖子内容包含图片的话，请将图片大小控制在20kb以内，以后升级服务器会无此限制，非常感谢您的谅解！<br>
+                    ②为了营造一个轻松的汉服交流空间，帖子内容请慎重考虑，不要涉及一些有影响的不健康的还有国家禁止的内容，非常感谢！
+                    </span>
                 </a-form-item>
                 <QuillEditorForArticle v-on:description="showMsgFromChild"></QuillEditorForArticle>
             </a-form>
@@ -45,7 +53,6 @@
             return {
                 visible: false,
                 userIdShow: false,
-                value: 1,
                 description: ''
             };
         },
